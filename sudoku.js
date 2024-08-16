e322a0f1224ee574a19730048d2fb1652c6c50da
arturka27 <arturka_tx@mail.ru>
2024-08-16 15:36:17 +0300

    feat: read

--- sudoku.js
+++ sudoku.js
@@ -1,7 +1,65 @@
+const { log } = require("console");
+const fs = require("fs");
+
 function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+  const puzzles = fs.readFileSync("./puzzles.txt", "utf-8").split("");
+  const allSudokus = [];
+  const allSudokusSorted = [];
+  for (let i = 0; i < puzzles.length; i++) {
+    let sudoku = [];
+    for (let i = 0; i < 81; i++) {
+      let nextEl = puzzles.shift();
+      if (nextEl === "\n" || nextEl === "\r") {
+        i -= 1;
+        continue;
+      }
+      sudoku.push(nextEl);
+    }
+    allSudokus.push(sudoku);
+  }
+  for (let index = 0; index < allSudokus.length; index++) {
+    const sudokuPlace = [];
+    const sudokuPlace1 = [];
+    const sudokuPlace2 = [];
+    const sudokuPlace3 = [];
+
+    for (let i = 0; i < allSudokus[index].length; i++) {
+      let block = [];
+      for (let i = 0; i < 9; i++) {
+        let nextEl = allSudokus[index].shift();
+        block.push(nextEl);
+      }
+      sudokuPlace1.push(block);
+    }
+
+    for (let i = 0; i < 9; i++) {
+      let block = [];
+      for (let j = 0; j < 9; j++) {
+        block.push(sudokuPlace1[j][i]);
+      }
+      sudokuPlace2.push(block);
+    }
+
+    for (let i = 0; i < 9; i++) {
+      sudokuPlace3.push([]);
+    }
+
+    for (let t = 0; t < 9; t += 3) {
+      for (let i = t; i < t + 3; i++) {
+        let level = 0;
+        for (let block = t; block < t + 3; block++) {
+          for (let j = level; j < 3 + level; j++) {
+            sudokuPlace3[block].push(sudokuPlace1[i][j]);
+          }
+          level += 3;
+        }
+      }
+    }
+    sudokuPlace.push(sudokuPlace1, sudokuPlace2, sudokuPlace3);
+    allSudokusSorted.push(sudokuPlace);
+  }
+
+  return allSudokusSorted;
 }
 
 function solve() {
