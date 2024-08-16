5fe487880cf6da1ee95b9de0480c797346f0fb68
Excomunicad0 <e_zhul@mail.ru>
2024-08-16 15:27:32 +0300

    feat:add prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -20,12 +20,9 @@ function isSolved() {
    */
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
+function prettyBoard(board) {
+  const table = board.map((el) => el.split('').map((n) => (n === '-' ? ' ' : n)));
+  console.table(table);
 }
 
 module.export = {
